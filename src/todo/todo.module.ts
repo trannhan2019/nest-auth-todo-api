80e19a04da276458afbadf129914d271426a871b
nhanphuong <trannhan.s3@gmail.com>
2024-10-10 19:19:39 +0700

    10.10.2024-home-fix vercel

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   providers: [PrismaService, TodoService],
